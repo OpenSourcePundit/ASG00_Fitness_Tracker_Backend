02f34fe22f1bdd5328623774b35b3898d652ca37
Shashank Kumar <shashank00098@gmail.com>
2023-12-20 14:00:55 +0530

    Update goal.models.js

--- backend/models/goal.models.js
+++ backend/models/goal.models.js
@@ -22,6 +22,10 @@ const goalSchema = mongoose.Schema({
         type:Number,
         required:true,
     },
+    totalCalBur:{
+        type:Number,
+        default:0
+    },
     status:{
         type:String,
         enum:["In Progress","Achieved","Failed","Awaiting"],
@@ -46,11 +50,38 @@ goalSchema.virtual("timePercentLeft").get(function(){
     
     return (currentDiffInDays/totalDiffInDays*100)        
 })
+
+goalSchema.virtual("totalCalorieBurned").get(async function(){
+    const allexercises = await Exercise.find({
+        "doneDate":{$gte:this.startDate, $lt: this.targetDate}
+    });
+    console.log(`AllExercises:${allexercises} `)
+
+     const cal = allexercises.reduce((total,curr)=>total+curr.calories,0)
+     console.log(`Caloriiiiees: ${cal}`)
+     this.totalCalBur = cal
+    await this.save()
+})
+
 goalSchema.virtual("achievementPercent").get(async function(){
-    const allexercises = await Exercise.find({});
-    const percent = {Exercises:allexercises}
-    console.log(percent)
-    return percent   
+    const allexercises = await Exercise.find({
+    "doneDate":{$gte:this.startDate, $lt: this.targetDate}
+    });
+
+    const totalCaloriesBurned = await allexercises.reduce((total,curr)=>total+curr.calories,0)
+    // console.log(`calories====${totalCaloriesBurned}`)
+
+    // console.log(`checktotalcalburnpercent: ${totalCaloriesBurned/this.targetCalorieValue*100}`)
+    if(totalCaloriesBurned/this.targetCalorieValue*100>100){
+        // console.log('truecase 100')
+        return  100
+    }
+        const ret =totalCaloriesBurned/this.targetCalorieValue*100
+
+        // console.log(`falsecase ${ret}`)
+        return await ret
+    
+    
 })
 
 const Goal = mongoose.model("Goal",goalSchema)
