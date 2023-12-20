c37449419e06917955788c601f4b25d957568893
Shashank Kumar <shashank00098@gmail.com>
2023-12-20 22:19:41 +0530

    test:vercel

--- backend/models/goal.models.js
+++ backend/models/goal.models.js
@@ -37,52 +37,54 @@ const goalSchema = mongoose.Schema({
     toObject:{virtuals:true}
 })
 
-goalSchema.virtual("timePercentLeft").get(function(){
-    const dayUnit=24*3600*1000;    
-    const currentDiffInMilliSec = new Date(this.targetDate).getTime()-new Date(Date.now()).getTime()
-    const currentDiffInDays = currentDiffInMilliSec/dayUnit;
+// goalSchema.virtual("timePercentLeft").get(function(){
+//     const dayUnit=24*3600*1000;    
+//     const currentDiffInMilliSec = new Date(this.targetDate).getTime()-new Date(Date.now()).getTime()
+//     const currentDiffInDays = currentDiffInMilliSec/dayUnit;
 
-    if(currentDiffInDays<0){
-        return 0
-    }
-    const totalDiffInMilliSec = new Date(this.targetDate).getTime()-new Date(this.startDate).getTime()
-    const totalDiffInDays = totalDiffInMilliSec/dayUnit;
+//     if(currentDiffInDays<0){
+//         return 0
+//     }
+//     const totalDiffInMilliSec = new Date(this.targetDate).getTime()-new Date(this.startDate).getTime()
+//     const totalDiffInDays = totalDiffInMilliSec/dayUnit;
     
-    return (currentDiffInDays/totalDiffInDays*100)        
-})
+//     return (currentDiffInDays/totalDiffInDays*100)        
+// })
 
-goalSchema.virtual("totalCalorieBurned").get(async function(){
-    const allexercises = await Exercise.find({
-        "doneDate":{$gte:this.startDate, $lt: this.targetDate}
-    });
-    console.log(`AllExercises:${allexercises} `)
+// goalSchema.virtual("totalCalorieBurned").get(async function(){
+//     const allexercises = await Exercise.find({
+//         "doneDate":{$gte:this.startDate, $lt: this.targetDate}
+//     });
+//     console.log(`AllExercises:${allexercises} `)
 
-     const cal = allexercises.reduce((total,curr)=>total+curr.calories,0)
-     console.log(`Caloriiiiees: ${cal}`)
-     this.totalCalBur = cal
-    await this.save()
-})
+//      const cal = allexercises.reduce((total,curr)=>total+curr.calories,0)
+//      console.log(`Caloriiiiees: ${cal}`)
+//      this.totalCalBur = cal
+//      await this.save();
+//     return  await cal ;
+    
+// })
 
-goalSchema.virtual("achievementPercent").get(async function(){
-    const allexercises = await Exercise.find({
-    "doneDate":{$gte:this.startDate, $lt: this.targetDate}
-    });
+// goalSchema.virtual("achievementPercent").get(async function(){
+//     const allexercises = await Exercise.find({
+//     "doneDate":{$gte:this.startDate, $lt: this.targetDate}
+//     });
 
-    const totalCaloriesBurned = await allexercises.reduce((total,curr)=>total+curr.calories,0)
-    // console.log(`calories====${totalCaloriesBurned}`)
+//     const totalCaloriesBurned = await allexercises.reduce((total,curr)=>total+curr.calories,0)
+//     // console.log(`calories====${totalCaloriesBurned}`)
 
-    // console.log(`checktotalcalburnpercent: ${totalCaloriesBurned/this.targetCalorieValue*100}`)
-    if(totalCaloriesBurned/this.targetCalorieValue*100>100){
-        // console.log('truecase 100')
-        return  100
-    }
-        const ret =totalCaloriesBurned/this.targetCalorieValue*100
+//     // console.log(`checktotalcalburnpercent: ${totalCaloriesBurned/this.targetCalorieValue*100}`)
+//     if(totalCaloriesBurned/this.targetCalorieValue*100>100){
+//         // console.log('truecase 100')
+//         return  100
+//     }
+//         const ret =totalCaloriesBurned/this.targetCalorieValue*100
 
-        // console.log(`falsecase ${ret}`)
-        return ret
+//         // console.log(`falsecase ${ret}`)
+//         return ret
     
     
-})
+// })
 
 const Goal = mongoose.model("Goal",goalSchema)
 export default Goal;
